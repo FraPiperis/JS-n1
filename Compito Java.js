14593de379f3a8507e3c1068452b0fd71221ad10
FraPiperis <FrancescaPiperis_00@outlook.it>
2024-11-18 16:23:02 +0100

    Add files via upload

---
+++ Compito Java.js
@@ -0,0 +1,75 @@
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/*
+NUMBER = indica un valore numerico 
+STRING = indica un valore testuale e in generale un valore definito attraverso gli apici 
+BOOLEAN = indica se un valore assegnato a un elemento è vero o falso 
+UNDEFINED = indica un valore ancora non definito 
+NULL = indica un valore nullo 
+*/
+
+/* ESERCIZIO 2
+ Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/
+
+/* 
+Un oggetto in JS è un elemento che indica un insieme di proprietà associate
+*/
+
+/* ESERCIZIO 3
+ Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/* 
+let num1 = 12 
+let num2 = 20 
+console.log (num1 + num2)
+*/
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/*
+lett x = 12 
+console.log (x)
+*/
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* 
+lett name = "Francesca" 
+console.log (name)
+ */
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/*
+let numero1 = 4
+let numero2 = x 
+x = 12 
+let risultato = x - numero1 
+console.log (risultato)
+*/
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+
+/* 
+let name1 = "john"
+let name2 = "John"
+console.log(name1 !== name2)
+let namemin = name2.toLowerCase("John")
+console.log(name1 === namemin)
+*/
+
